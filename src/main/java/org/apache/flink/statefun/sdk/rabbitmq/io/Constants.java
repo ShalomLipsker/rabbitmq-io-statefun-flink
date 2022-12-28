cf5009f6d8746c2c21d4a27cfca3709a3848f52e
user <user@a.com>
2022-12-28 14:24:27 +0200

    first try to embedded

--- src/main/java/org/apache/flink/statefun/sdk/rabbitmq/io/Constants.java
+++ src/main/java/org/apache/flink/statefun/sdk/rabbitmq/io/Constants.java
@@ -1,12 +1,10 @@
 package org.apache.flink.statefun.sdk.rabbitmq.io;
 
-import com.google.protobuf.StringValue;
 import org.apache.flink.statefun.sdk.EgressType;
 import org.apache.flink.statefun.sdk.IngressType;
 import org.apache.flink.statefun.sdk.io.EgressIdentifier;
 import org.apache.flink.statefun.sdk.io.IngressIdentifier;
 import org.apache.flink.statefun.sdk.reqreply.generated.TypedValue;
-import com.google.protobuf.Message;
 
 public class Constants {
   private static final String NAMESPACE = "io.statefun.rabbitmq";
@@ -21,9 +19,6 @@ public class Constants {
       new IngressIdentifier<>(TypedValue.class, NAMESPACE, INGRESS);
 
   public static final String DEFAULT_INGRESS_TYPE = "io.statefun.types/string";
-  public static final String STATEFUN_CONTENT_TYPE_PREFIX = "application/vnd.";
-  public static final String RABBITMQ_EGRESS_RECORD = "io.statefun.rabbitmq/EgressRecord";
-
   private Constants() {
     throw new UnsupportedOperationException("Should not be instantiated.");
   }
--- src/main/java/org/apache/flink/statefun/sdk/rabbitmq/io/binders/TypedValueAsStringSchema.java
+++ src/main/java/org/apache/flink/statefun/sdk/rabbitmq/io/binders/TypedValueAsStringSchema.java
@@ -2,23 +2,24 @@ package org.apache.flink.statefun.sdk.rabbitmq.io.binders;
 
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.statefun.sdk.rabbitmq.io.Constants;
 import org.apache.flink.statefun.sdk.reqreply.generated.TypedValue;
 import com.google.protobuf.ByteString;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import java.io.IOException;
 
 
+
 public class TypedValueAsStringSchema implements DeserializationSchema<TypedValue>, SerializationSchema<TypedValue> {
     @Override
     public TypedValue deserialize(byte[] bytes) throws IOException {
         ByteString bs = ByteString.copyFrom(bytes);
-        String STRING_TYPENAME = "io.statefun.types/string";
         TypedValue typedValue =
                 TypedValue
                         .newBuilder()
                         .setValue(bs).
                         setHasValue(true).
-                        setTypename(STRING_TYPENAME)
+                        setTypename(Constants.DEFAULT_INGRESS_TYPE)
                         .build();
         return typedValue;
     }
